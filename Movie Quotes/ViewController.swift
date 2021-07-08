d758d561cafe144c7adec681b435615d2de1510b
Loki <45338967+rhit-strainlw@users.noreply.github.com>
2021-07-07 21:13:44 -0400

    Initial Commit

---
+++ Movie Quotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie Quotes
+//
+//  Created by Loki Strain on 7/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
