a3f89611ef08fd4e6b44014ebd52d545fc984045
Loki <45338967+rhit-strainlw@users.noreply.github.com>
2021-07-11 20:29:31 -0400

    Table Ready for Dialog

---
+++ Movie Quotes/MovieQuote.swift
@@ -0,0 +1,18 @@
+//
+//  MovieQuote.swift
+//  Movie Quotes
+//
+//  Created by Loki Strain on 7/11/21.
+//
+
+import Foundation
+
+class MovieQuote {
+    var quote: String
+    var movie: String
+    
+    init(quote: String, movie: String){
+        self.quote = quote
+        self.movie = movie
+    }
+}
--- Movie Quotes/MovieQuotesTableViewController.swift
+++ Movie Quotes/MovieQuotesTableViewController.swift
@@ -12,16 +12,33 @@ class MovieQuotesTableViewController: UITableViewController {
     
     let movieQuoteCellIdentifier = "MovieQuoteCell"
     
-    var names = ["Loki","Jade","Larry"]
+    // var names = ["Loki","Jade","Larry"]
+    var movieQuotes = [MovieQuote]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        movieQuotes.append(MovieQuote(quote: "I Worked", movie: "Movie 1"))
+        movieQuotes.append(MovieQuote(quote: "I Worked again", movie: "Movie 2"))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(showAddQuoteDialog))
+        
+    }
+    
+    @objc func showAddQuoteDialog(){
+        print("You pressed it")
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return names.count
+        return movieQuotes.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: movieQuoteCellIdentifier, for: indexPath)
         
-        cell.textLabel?.text = names[indexPath.row]
+        cell.textLabel?.text = movieQuotes[indexPath.row].quote
+        cell.detailTextLabel?.text = movieQuotes[indexPath.row].movie
+
+        
         return cell
     }
 }
