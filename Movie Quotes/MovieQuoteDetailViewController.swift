de811272334573db7b3bdd59f4a459837840a662
Loki <45338967+rhit-strainlw@users.noreply.github.com>
2021-08-02 19:45:38 -0400

    Edit used Auth

--- Movie Quotes/MovieQuoteDetailViewController.swift
+++ Movie Quotes/MovieQuoteDetailViewController.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 import Firebase
+import FirebaseAuth
 
 class MovieQuoteDetailViewController: UIViewController {
     
@@ -21,7 +22,7 @@ class MovieQuoteDetailViewController: UIViewController {
     override func viewDidLoad(){
         
         super.viewDidLoad()
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.edit, target: self, action: #selector(showEditDialog))
+        
         
     }
     
@@ -39,6 +40,19 @@ class MovieQuoteDetailViewController: UIViewController {
                 return
             }
             self.movieQuote = MovieQuote(documentSnapshot: documentSnapshot!)
+            
+//            Decide if we can edit or not
+            if (Auth.auth().currentUser!.uid == self.movieQuote?.author){
+                
+                self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.edit, target: self, action: #selector(self.showEditDialog))
+                
+            }else{
+                self.navigationItem.rightBarButtonItem = nil
+            }
+            
+            
+            
+            
             self.updateView()
             }
         }
