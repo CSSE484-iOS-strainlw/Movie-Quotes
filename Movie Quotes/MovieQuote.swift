7eb5a835e55b17a8217b22d5bbd6f81fca75d61a
Loki <45338967+rhit-strainlw@users.noreply.github.com>
2021-08-02 19:37:54 -0400

    Delete as Auth

--- Movie Quotes/MovieQuote.swift
+++ Movie Quotes/MovieQuote.swift
@@ -12,15 +12,13 @@ class MovieQuote {
     var quote: String
     var movie: String
     var id: String?
+    var author: String
     
-    init(quote: String, movie: String){
-        self.quote = quote
-        self.movie = movie
-    }
     init(documentSnapshot: DocumentSnapshot){
         self.id = documentSnapshot.documentID
         let data = documentSnapshot.data()!
         self.quote = data["quote"] as! String
         self.movie = data["movie"] as! String
+        self.author = data["author"] as! String
     }
 }
